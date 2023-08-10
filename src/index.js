1e29f4cd8d53bf3906b8ce7063b1d8b54c43ff70
Margarita Kachalko <margarita.kachalko@gmail.com>
2023-08-10 17:16:56 +0300

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -38,7 +38,55 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+  const arr = [];
+  const arrStr = [];
+
+  const toParts = (str, num, arr) => {
+
+    for (let i = 0; i < str.length; i += num) {
+      arr.push(str.substr(i, num));
+    };
+
+  }
+
+  const toMorse = (num) => {
+    switch (num) {
+      case '00':
+        return '';
+      case '10':
+        return '.';
+      case '11':
+        return '-'
+    }
+  }
+
+  const toStr = (morse) => {
+    if (morse == ' ') {
+      return ' '
+    } else {
+      return MORSE_TABLE[morse]
+    }
+  }
+
+  toParts(expr, 10, arr);
+  
+  arr.forEach (item => {
+    const arrMorse = [];
+    let strMorse;
+
+    if(item == '**********') {
+      arrStr.push(' ');
+    } else {
+      toParts(item, 2, arrMorse);
+      strMorse = arrMorse.map(toMorse).join('')
+      arrStr.push(strMorse);
+    }
+
+  })
+
+
+  return arrStr.map(toStr).join('');
+
 }
 
 module.exports = {
